12988d758f156843713a80925f6711a582b2b3b5
geelya <93830274+geelya@users.noreply.github.com>
2021-12-26 22:01:25 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -269,7 +269,7 @@ class Autoreply:
                 geturl,tid=Autoreply.getonelink(todaylist)
                 reply_news=Autoreply.getreply()
                 res=Autoreply.getmatch(geturl,cookies)
-                sleeptime=random.randint(300,600)
+                sleeptime=random.randint(1024,1500)
                 au=Autoreply.postreply(cookies,res,reply_news,tid)
                 if au=='回复成功':
                     print('第'+str(ge+1)+'个账号回复成功')
